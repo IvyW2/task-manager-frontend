7e188473260423263cbbe9b983ad16f03d9ac235
IvyW2 <ivywambugha2@gmail.com>
2023-04-14 01:12:47 +0300

    add user projects

---
+++ src/components/UserDataProvider.js
@@ -0,0 +1,44 @@
+import React, { createContext, useEffect, useState } from "react"
+
+// create the context object
+const UserContext = createContext()
+const API = 'http://127.0.0.1:9292'
+
+
+// create the context provider (component)
+function UserDataProvider({ children }) {
+    const [user, setUser] = useState(null)
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem('user');
+        if (loggedInUser) {
+          const foundUser = JSON.parse(loggedInUser);
+        //   console.log(foundUser)
+          setUser(foundUser);
+        }
+    }, []);
+
+    useEffect(()=>{
+        if(user){
+            fetch(`${API}/projects/${user.id}`)
+            .then(resp=>resp.json())
+            .then(data=>{
+                // console.log(...data)
+                setProjects((projects)=>data.my_projects.concat(data.part_of))
+                // console.log(projects)
+            })
+        }
+    },[user])
+    // console.log(projects)
+    const value = [user, setUser,API,projects,setProjects]
+    
+    return (
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+// export
+export { UserContext, UserDataProvider }
