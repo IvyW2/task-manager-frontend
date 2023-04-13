e57a7ce6c9776b658012f9e5e7cc11e8f6cef5ef
IvyW2 <ivywambugha2@gmail.com>
2023-04-14 01:02:39 +0300

    added MyProject component

---
+++ src/components/MyProjects.js
@@ -0,0 +1,69 @@
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ProjectRow from './ProjectRow';
+import { UserContext } from './UserDataProvider';
+
+function MyProjects(props) {
+
+    const [user,setUser,API,projects] = useContext(UserContext)
+
+    const projectList = projects?.map((project,index)=>{
+        return(
+            <ProjectRow
+            key={`projectList`+project.id}
+            index={index+1}
+            project={project}
+            />
+        )
+    })
+
+
+
+    if(projects.length>0){
+        return(
+            <div className='container mt-5 bg-body-tertiary'>
+                <div className='container'>
+                    <Link to='/project-form' className='mx-auto btn btn-outline-success'>Add New Project</Link>
+                </div>
+                <h2>Here are some of your projects</h2>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Members</th>
+                            <th scope="col">Due Date</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {projectList}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div>
+                <div className="row justify-content-center mt-5">
+                    <div className="card col-lg-4 col-sm-12 col-md-6">
+                        <img src="https://img.icons8.com/color/96/null/nothing-found.png"
+                            className="card-img-top" alt="nothing" style={{objectFit: "inherit"}}/>
+                        <div className="card-body">
+                            <p className="card-text">You have no projects created yet!</p>
+                        </div>
+                        <div className='container'>
+                            <Link to='/project-form' className='mx-auto btn btn-outline-success'>Add New Project</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MyProjects;
