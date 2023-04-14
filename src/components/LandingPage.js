e5a678c05699ccc56db49b4ef7bc1ad798adda09
IvyW2 <ivywambugha2@gmail.com>
2023-04-14 05:15:29 +0300

    image change

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../stylesheets/LandingPage.css'
-import image from '../images/blue_wall.jpg'
+import image from '../images/business-planning-task-management-concept-260nw-1987578881(1).png'
+
 
 function LandingPage(props) {
     // return (
@@ -14,9 +15,9 @@ function LandingPage(props) {
     return (
         <div className="landing-page-container">
           <div className="landing-page-text">
-            <h1 style={{ fontFamily: 'Roboto Slab, serif' }}>Welcome to PROJECT-APP</h1>
-            <p style={{ fontFamily: 'Open Sans, sans-serif' }}>Manage your projects efficiently with our app</p>
-            <p style={{ fontFamily: 'Open Sans, sans-serif' }}>Sign up or log in to get started</p>
+            <h1 style={{ fontFamily: 'Roboto Slab, serif' }}>Welcome to TASK MANAGER</h1>
+            <p style={{ fontFamily: 'Open Sans, sans-serif' }}>Let us Manage you tasks efficiently</p>
+            <p style={{ fontFamily: 'Open Sans, sans-serif' }}>Log in or Sign up to get started</p>
           </div>
           <img src={image} alt="Project management app" className="landing-page-image" />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,14 +24,14 @@ function Navbar({currentUser,isLoggedIn,setIsLoggedIn}){
         <nav className="navbar navbar-expand-lg bg-body-secondary">
                 <div className="container-fluid">
                     <Link className="navbar-brand h1" to="/"
-                    >PROJECT-APP</Link>
+                    >TASK MANAGER</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className={`${ulClasses} justify-content-center`}>
                             {user?<li className="nav-item">
-                                <Link className="nav-link mx-5" aria-current="page" to="/my-projects">MY-PROJECTS</Link>
+                                <Link className="nav-link mx-5" aria-current="page" to="/my-projects">MY-TASKS</Link>
                             </li>:null}
                         </ul>
                     </div>
